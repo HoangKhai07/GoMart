19de2e3e1ec410948df9328e621514428c91bd8a
KhaiNoob <danhhoangkhai07@gmail.com>
2025-02-24 22:25:30 +0700

    add delete category controller

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,4 +1,6 @@
 import CategoryModel from "../model/category.model.js";
+import SubCategoryModel from "../model/subCategory.model.js";
+import ProductModel from "../model/product.model.js";
 
 export const AddCategoryController = async(request, response) => {
     try {
@@ -9,6 +11,15 @@ export const AddCategoryController = async(request, response) => {
                 message: "Vui lòng nhập đầy đủ thông tin của danh mục sản phẩm",
                 error: true,
                 sucess: false
+            })
+        }
+
+        const existingCategory = await CategoryModel.findOne({name: name})
+        if(existingCategory){
+            return res.status(400).json({
+                message: "Tên danh mục đã tồn tại, vui lòng chọn tên khác!",
+                error: true,
+                success: false
             })
         }
         
@@ -73,14 +84,7 @@ export const updateCategoryController = async (req, res) => {
             })
         }
 
-        const existingCategory = await CategoryModel.findOne({name: name})
-        if(existingCategory){
-            return res.status(400).json({
-                message: "Tên danh mục đã tồn tại, vui lòng chọn tên khác!",
-                error: true,
-                success: false
-            })
-        }
+       
        
 
         const update = await CategoryModel.findOneAndUpdate(
@@ -107,9 +111,45 @@ export const updateCategoryController = async (req, res) => {
     }
 }
 
-export const deleteCategoryController = async (res, rep) => {
+export const deleteCategoryController = async (req, res) => {
     try {
+
+        const {_id} = req.body
+
+        const checkSubCategory = await SubCategoryModel.find({
+            category: {
+                "$in": [_id]
+            }
+        }).countDocuments()
+
+        const checkProduct = await ProductModel.find({
+            category: {
+                "$in": [_id]
+            }
+        }).countDocuments()
+
+        if(checkSubCategory > 0 || checkProduct > 0){
+            return res.status(400).json({
+                message: "Danh mục này đang có danh mục con hoặc sản phẩm, vui lòng xóa danh mục con trước!",
+                error: true,
+                success: false
+            })
+        }
+
+        const deleteCategory = await CategoryModel.deleteOne({_id: _id})
+        return res.json({
+            message: "Xóa danh mục sản phẩm thành công!",
+            error: false,
+            success: true,
+            data: deleteCategory
+        })
     } catch (error) {
+        return res.status(500).json({
+            message: error.message || error,
+            success: false,
+            error: true
+
+        })
         
     }
 }
