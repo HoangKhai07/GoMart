88f990d936fc600901d09cfe1deb129984a1e40f
KhaiNoob <danhhoangkhai07@gmail.com>
2025-02-27 21:30:45 +0700

    addupdate and delete subcategory controller

--- controllers/subCategory.controller.js
+++ controllers/subCategory.controller.js
@@ -1,3 +1,4 @@
+import ProductModel from "../model/product.model.js";
 import SubCategoryModel from "../model/subCategory.model.js";
 
 export const AddSubCategoryController = async (req, res) => {
@@ -54,6 +55,71 @@ export const getSubCategoryController = async (req, res) => {
         return res.status(500).json({
             message: error.message || error,
             error: true,
+            success: false
+        })
+    }
+}
+
+export const updateSubCategoryController = async (req, res) => {
+
+    try {
+        const {_id, name, image, category} = req.body
+
+    const update = await SubCategoryModel.findOneAndUpdate(
+        {_id: _id},
+        {
+            name,
+            image,
+            category
+        },
+        {new: true}
+    )
+
+    return res.json({
+        message: "Cập nhật danh mục thành công!",
+        data: update,
+        error: false,
+        success: true
+    })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || error,
+            success: false
+        })
+    }
+}
+
+export const deleteSubCategoryController = async (req, res) => {
+    try {
+        const {_id} = req.body
+
+        const checkProduct = await ProductModel.find({
+            category: {
+                "$in": [_id]
+            }
+        }).countDocuments()
+
+        if(checkProduct > 0){
+            return res.status(400).json({
+                message: "Danh mục này đang có sản phẩm. Vui lòng xóa sản phẩm trước!",
+                success: false,
+                error: true
+            })
+        }
+
+        const deleteSubCategory = await SubCategoryModel.deleteOne({_id: _id})
+
+        return res.json({
+            message: "Xóa danh mục con thành công!",
+            success: true,
+            error: false,
+            data: deleteSubCategory
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: message || error,
+            error: true, 
             success: false
         })
     }
