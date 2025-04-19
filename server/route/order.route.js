093c9cc4bdc963edcb7841b1a861170c36368f7c
KhaiNoob <danhhoangkhai07@gmail.com>
2025-04-19 14:04:58 +0700

    Update get order detail controller and fix some bug

--- server/route/order.route.js
+++ server/route/order.route.js
@@ -2,7 +2,7 @@ import { Router} from 'express'
 import auth from '../middleware/auth.js'
 import adminAuth from '../middleware/admin.js'
 
-import { CashOnDeliveryPaymentController, getOrderDetailsController, paymentController, webhookStripe, updateOrderStatusController, getAllOrdersController } from '../controllers/order.controller.js'
+import { CashOnDeliveryPaymentController, getOrderDetailsController, paymentController, webhookStripe, updateOrderStatusController, getAllOrdersController, getDetailsController } from '../controllers/order.controller.js'
 
 const orderRouter = Router()
 
@@ -10,8 +10,9 @@ orderRouter.post('/cash-on-delivery', auth, CashOnDeliveryPaymentController)
 orderRouter.post('/checkout', auth, paymentController)
 orderRouter.post('/webhook', webhookStripe)
 orderRouter.get('/order-list', auth, getOrderDetailsController)
+orderRouter.get('/detail/:orderId', auth, getDetailsController)
 
-// Thêm route cho admin
+// route manage for admin
 orderRouter.get('/all-orders', auth, adminAuth, getAllOrdersController)
 orderRouter.put('/update-status', auth, adminAuth, updateOrderStatusController)
 
